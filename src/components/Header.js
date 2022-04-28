0bf37b1c1c015f5511055da4d85584a71428fbc6
Joel-Adving <joel.adving@gmail.com>
2022-04-28 17:58:02 +0200

    ui improvementes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import { useAuth } from '../hooks/useAuth'
 
@@ -5,33 +6,36 @@ export default function Header() {
     const { user, signin, logout } = useAuth()
 
     return (
-        <header className="p-6 bg-background-dark">
-            <nav className="flex items-center justify-between w-full max-w-5xl mx-auto ">
-                <Link href="/">
-                    <h2 className="text-2xl font-bold cursor-pointer text-text">GRUS</h2>
-                </Link>
+        <header className="bg-background-darkest font-BeaufortBold text-text  border-t-[2.5px] border-b-[1px] border-neutral-700 border-t-border-light">
+            <nav className="container flex justify-between w-full px-3 py-4 mx-auto sm:px-0">
+                <div className="flex items-center">
+                    <h2 className="mr-3 text-xl sm:mr-12 text-text-highlight border-victory">GRUS</h2>
+                    <div className="flex space-x-2 sm:space-x-6">
+                        <Link href="/">HOME</Link>
+                        <Link href="#">PLAYLIST</Link>
+                    </div>
+                </div>
 
                 {!user && (
-                    <button className="px-3 py-[3px] text-sm text-white bg-green-bg" onClick={() => signin()}>
-                        Sign in
+                    <button className="" onClick={() => signin()}>
+                        SIGN IN
                     </button>
                 )}
                 {user && (
-                    <div className="flex flex-col sm:flex-row">
-                        <div className="flex items-center bg-background">
-                            <img
-                                className="border-r-2 border-highlight w-7 h-7"
+                    <div className="flex items-center ">
+                        <button className="" onClick={() => logout()}>
+                            SIGN OUT
+                        </button>
+                        <div className="w-12 h-12 mx-3 border-[2.5px] rounded-full border-victory">
+                            <Image
+                                className="rounded-full "
                                 src={user.photoURL}
                                 alt="User profile image"
+                                height={48}
+                                width={48}
                             />
-                            <h3 className="mx-3 text-md text-highlight">{user.name}</h3>
                         </div>
-                        <button
-                            className="px-4 py-[3px] text-sm text-white bg-green-bg min-w-max"
-                            onClick={() => logout()}
-                        >
-                            Sign Out
-                        </button>
+                        <h3 className=" text-text-highlight">{user.name}</h3>
                     </div>
                 )}
             </nav>
--- src/pages/index.js
+++ src/pages/index.js
@@ -13,11 +13,11 @@ export default function Home({ data }) {
     return (
         <>
             <div className="min-h-screen bg-gradient-to-b from-background-darkest via-background-darkest to-background-lightest">
-                <div className="flex flex-col items-center justify-center py-12 text-2xl sm:text-4xl md:text-5xl bg-background-darkest font-frizQuad text-gold-light">
-                    <div className="flex">
+                <div className="flex flex-col items-center justify-center py-6 pb-10 text-2xl sm:text-4xl md:text-5xl bg-background-darkest font-frizQuad text-gold-light">
+                    {/* <div className="flex">
                         <h1>LEAGUE</h1>
                         <h3 className="self-end mb-2 ml-1 text-xs sm:text-xl text-gold-light">of</h3>
-                    </div>
+                    </div> */}
                     <h1>GRABBARNA</h1>
                     <div className="flex">
                         <h1>GRUS</h1>
