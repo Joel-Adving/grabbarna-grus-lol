b807d9a313b11fe56b832607103496f0f3f0a6a8
Joel-Adving <joel.adving@gmail.com>
2022-04-28 17:18:31 +0200

    implemented auth and firebase setup

---
+++ src/components/Header.js
@@ -0,0 +1,40 @@
+import Link from 'next/link'
+import { useAuth } from '../hooks/useAuth'
+
+export default function Header() {
+    const { user, signin, logout } = useAuth()
+
+    return (
+        <header className="p-6 bg-background-dark">
+            <nav className="flex items-center justify-between w-full max-w-5xl mx-auto ">
+                <Link href="/">
+                    <h2 className="text-2xl font-bold cursor-pointer text-text">GRUS</h2>
+                </Link>
+
+                {!user && (
+                    <button className="px-3 py-[3px] text-sm text-white bg-green-bg" onClick={() => signin()}>
+                        Sign in
+                    </button>
+                )}
+                {user && (
+                    <div className="flex flex-col sm:flex-row">
+                        <div className="flex items-center bg-background">
+                            <img
+                                className="border-r-2 border-highlight w-7 h-7"
+                                src={user.photoURL}
+                                alt="User profile image"
+                            />
+                            <h3 className="mx-3 text-md text-highlight">{user.name}</h3>
+                        </div>
+                        <button
+                            className="px-4 py-[3px] text-sm text-white bg-green-bg min-w-max"
+                            onClick={() => logout()}
+                        >
+                            Sign Out
+                        </button>
+                    </div>
+                )}
+            </nav>
+        </header>
+    )
+}
