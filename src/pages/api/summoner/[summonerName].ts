0e4d16195a51fb7f6f44885abdf22d4439fff4a0
Joel-Adving <joel.adving@gmail.com>
2022-05-02 21:05:50 +0200

    removed api enpoints for maintenance

--- src/pages/api/summoner/[summonerName].ts
+++ src/pages/api/summoner/[summonerName].ts
@@ -1,16 +1,18 @@
-import { matches, matchHistory, summoner } from '../../../util/riotFetch'
-import type { NextApiRequest, NextApiResponse } from 'next'
+// import { matches, matchHistory, summoner } from '../../../util/riotFetch'
+// import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const { summonerName } = req.query
-    const resSummoner = await summoner(summonerName.toString())
-    // const resMatchHistory = await matchHistory(resSummoner.puuid)
-    // const resMatches = await matches(resMatchHistory)
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+//     const { summonerName } = req.query
+//     const resSummoner = await summoner(summonerName.toString())
+//     // const resMatchHistory = await matchHistory(resSummoner.puuid)
+//     // const resMatches = await matches(resMatchHistory)
 
-    res.json(
-        JSON.stringify({
-            summoner: resSummoner,
-            // matchHistory: resMatches,
-        })
-    )
-}
+//     res.json(
+//         JSON.stringify({
+//             summoner: resSummoner,
+//             // matchHistory: resMatches,
+//         })
+//     )
+// }
+
+export {}
--- src/pages/api/summoners.ts
+++ src/pages/api/summoners.ts
@@ -1,9 +1,11 @@
-import { summoners } from '../../util/riotFetch'
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { getCollection } from '../../firebase/getCollection'
-import { Summoner } from '../../util/types'
+// import { summoners } from '../../util/riotFetch'
+// import type { NextApiRequest, NextApiResponse } from 'next'
+// import { getCollection } from '../../firebase/getCollection'
+// import { Summoner } from '../../util/types'
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const data: Array<Summoner> = await getCollection('summoners')
-    res.json(data)
-}
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+//     const data: Array<Summoner> = await getCollection('summoners')
+//     res.json(data)
+// }
+
+export {}
