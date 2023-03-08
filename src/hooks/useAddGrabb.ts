cfb2d888302797fd2392adcdb66d8bd1b81fd7f6
Joel Adving <joel.adving@gmail.com>
2023-03-09 00:09:15 +0100

    bugix

--- src/hooks/useAddGrabb.ts
+++ src/hooks/useAddGrabb.ts
@@ -1,35 +1,26 @@
-import { doc, setDoc, where } from 'firebase/firestore'
 import React, { useState } from 'react'
-import { db } from '@/lib/firebase/config'
-import { getCollection } from '@/lib/firebase/getCollection'
-import { summoner } from '@/utils/riotFetch'
 
 export const useAddGrabb = () => {
-  const [summonerName, setSummonerName] = useState('')
-  const [message, setMessage] = useState<string | null>(null)
-  const [loading, setLoading] = useState(false)
-
-  const handleSubmit = async (e: React.SyntheticEvent) => {
-    e.preventDefault()
-    setMessage(null)
-    setLoading(true)
-
-    const res = await getCollection('summoners', [where('name', '==', summonerName)])
-
-    if (res?.length) {
-      setMessage('Summoner already exists in database')
-      setLoading(false)
-      setSummonerName('')
-    } else {
-      const resSummoner = await summoner(summonerName)
-      await setDoc(doc(db, 'summoners', resSummoner.id), resSummoner)
-      setMessage(`Added ${summonerName} to database`)
-      setSummonerName('')
-      setLoading(false)
-    }
-  }
-
-  const handleInputChange = (e: React.FormEvent<HTMLInputElement>) => setSummonerName(e.currentTarget.value)
-
-  return { message, loading, summonerName, handleSubmit, handleInputChange }
+  //   const [summonerName, setSummonerName] = useState('')
+  //   const [message, setMessage] = useState<string | null>(null)
+  //   const [loading, setLoading] = useState(false)
+  //   const handleSubmit = async (e: React.SyntheticEvent) => {
+  //     e.preventDefault()
+  //     setMessage(null)
+  //     setLoading(true)
+  //     const res = await getCollection('summoners', [where('name', '==', summonerName)])
+  //     if (res?.length) {
+  //       setMessage('Summoner already exists in database')
+  //       setLoading(false)
+  //       setSummonerName('')
+  //     } else {
+  //       const resSummoner = await summoner(summonerName)
+  //       await setDoc(doc(db, 'summoners', resSummoner.id), resSummoner)
+  //       setMessage(`Added ${summonerName} to database`)
+  //       setSummonerName('')
+  //       setLoading(false)
+  //     }
+  //   }
+  //   const handleInputChange = (e: React.FormEvent<HTMLInputElement>) => setSummonerName(e.currentTarget.value)
+  //   return { message, loading, summonerName, handleSubmit, handleInputChange }
 }
--- src/hooks/useRealTimeCollection.ts
+++ src/hooks/useRealTimeCollection.ts
@@ -1,19 +1,17 @@
-import { collection, onSnapshot } from 'firebase/firestore'
-import { db } from '../lib/firebase/config'
+// import { collection, onSnapshot } from 'firebase/firestore'
+// import { db } from '../lib/firebase/config'
 import { useState } from 'react'
 
 export function useRealTimeCollection(col: string) {
-  const [data, setData] = useState<Array<any> | null>(null)
-
-  const colRef = collection(db, col)
-  const unsubscribe = onSnapshot(colRef, (querySnapshot) => {
-    const docs: Array<any> = []
-    querySnapshot.forEach((doc) => {
-      docs.push(doc.data())
-    })
-    setData(docs)
-    return () => unsubscribe()
-  })
-
-  return { data }
+  //   const [data, setData] = useState<Array<any> | null>(null)
+  //   const colRef = collection(db, col)
+  //   const unsubscribe = onSnapshot(colRef, (querySnapshot) => {
+  //     const docs: Array<any> = []
+  //     querySnapshot.forEach((doc) => {
+  //       docs.push(doc.data())
+  //     })
+  //     setData(docs)
+  //     return () => unsubscribe()
+  //   })
+  //   return { data }
 }
