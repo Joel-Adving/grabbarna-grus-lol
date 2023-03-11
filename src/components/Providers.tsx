22145b92badf16f0e9eb423d5e51761fac1d131a
Joel Adving <joel.adving@gmail.com>
2023-03-11 16:15:52 +0100

    longer revalidation time

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -2,6 +2,13 @@
 
 import { SWRConfig } from 'swr'
 
+const swrConf = {
+  dedupingInterval: 3600000, // one hour
+  revalidateOnFocus: false,
+  revalidateOnReconnect: false,
+  provider: () => new Map()
+}
+
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <SWRConfig value={{ dedupingInterval: 600000, provider: () => new Map() }}>{children}</SWRConfig>
+  return <SWRConfig value={swrConf}>{children}</SWRConfig>
 }
